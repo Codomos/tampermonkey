9fc03376303cf9de63845f1cd73458cd0b39d88e
albert <albert_lyv@outlook.com>
2023-03-14 21:02:06 +0800

    change extension

---
+++ csdnNoLogin.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         CSDN no login
+// @version      0.7
+//author       albert
+//
+
+
+// @description  try to take over the world!
+// @license      MIT
+// @icon         https://www.csdn.net/favicon.ico
+
+// @match        *://*.csdn.net/*
+// @require      tampermonkey://vendor/jquery.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+     $("pre,code").css("user-select","auto");
+})();
